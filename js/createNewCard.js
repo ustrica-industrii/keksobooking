f7286866188e56fe9d8c0aee7f60da58e9978514
slava <skat_113@mail.ru>
2023-06-14 00:36:51 +0300

    1

--- js/createNewCard.js
+++ js/createNewCard.js
@@ -5,40 +5,63 @@ import { similarPosters } from './data.js';
 const template = document.querySelector('#card').content;
 const newCard = template.querySelector('.popup');
 const map = document.querySelector('#map-canvas');
+const similarCard = newCard.cloneNode(true);
 
 for(let i=0; i < similarPosters.length; i++){
-    const similarCard = newCard.cloneNode(true);
+  const currentPoster = similarPosters[i];
 
-    const cardTitle = similarPosters[i].offer.title;
-    const similarCardTitle = similarCard.querySelector('.popup__title');
-    similarCardTitle.textContent = cardTitle;
+  const cardTitle = currentPoster.offer.title;
+  const similarCardTitle = similarCard.querySelector('.popup__title');
+  similarCardTitle.textContent = cardTitle;
 
-   // const cardAddress = similarPosters[i].location.x + ',' + similarPosters[i].location.y;
-   // const similarCardAddress = similarCard.querySelector('.popup__text--addres')
-   // similarCardAddress.textContent = cardAddress;
-    const cardPrice = similarPosters[i].offer.price;
-    const similarCardPrice = similarCard.querySelector('.popup__text--price');
-    similarCardPrice.textContent = cardPrice + '₽/ночь';
+  const cardAddress = currentPoster.offer.address.x + ',' + currentPoster.offer.address.y;
+  const similarCardAddress = similarCard.querySelector('.popup__text--address')
+  similarCardAddress.textContent = cardAddress;
 
-    const cardType = similarPosters[i].offer.type;
-    const similarCardType = similarCard.querySelector('.popup__type');
-    similarCardType.textContent = cardType;
+  const cardPrice = currentPoster.offer.price;
+  const similarCardPrice = similarCard.querySelector('.popup__text--price');
+  similarCardPrice.textContent = cardPrice + '₽/ночь';
 
-    const cardRoms = similarPosters[i].offer.rooms;
-    const cardQuests = similarPosters[i].offer.quests;
-    const similarCardRoomsAndQuests = similarCard.querySelector('.popup__text--capacity');
-    similarCardRoomsAndQuests.textContent = cardRoms + ' комнаты для ' + cardQuests + ' гостей';
+  const cardType = currentPoster.offer.type;
+  const similarCardType = similarCard.querySelector('.popup__type');
+  similarCardType.textContent = cardType;
 
-    const cardCheckin = similarPosters[i].offer.chekin;
-    const cardCheckout = similarPosters[i].offer.checkout;
-    const similarCardTimes = similarCard.querySelector('.popup__text--time');
-    similarCardTimes.textContent = 'Заезд после ' + cardCheckin + ', выезд до ' + cardCheckout;
+  const cardRoms = currentPoster.offer.rooms;
+  const cardQuests = currentPoster.offer.quests;
+  const similarCardRoomsAndQuests = similarCard.querySelector('.popup__text--capacity');
+  similarCardRoomsAndQuests.textContent = cardRoms + ' комнаты для ' + cardQuests + ' гостей';
+
+  const cardCheckin = currentPoster.offer.chekin;
+  const cardCheckout = currentPoster.offer.checkout;
+  const similarCardTimes = similarCard.querySelector('.popup__text--time');
+  similarCardTimes.textContent = 'Заезд после ' + cardCheckin + ', выезд до ' + cardCheckout;
+
+  const cardFeatures = currentPoster.offer.features;
+  const similarCardFeatures = similarCard.querySelector('.popup__features');
+  similarCardFeatures.textContent = cardFeatures;
+
+  const cardDescription = currentPoster.offer.description;
+  const similarCardDescription = similarCard.querySelector('.popup__description');
+  similarCardDescription.textContent = cardDescription;
+
+  // const cardPhoto = currentPoster.offer.photos;
+  // const similarCardPhotos = similarCard.querySelector('.popup__photos');
+  // const photo = similarCard.querySelector('.popup__photo');
+  // const one = photo.cloneNode(true)
+  // for(let j=0;j<cardPhoto.length;j++){
+  //   one[j].src=cardPhoto[j];
+  //   similarCardPhotos.appendChild(one[j])
+  // }
 
 
-    map.appendChild(similarCard)
+
+  const cardAvatar = currentPoster.author.avatar;
+  const similarCardAvatar = similarCard.querySelector('.popup__avatar');
+  similarCardAvatar.src = cardAvatar;
+
+
 }
-
-// Выведите адрес offer.address в блок .popup__text--address.
+map.appendChild(similarCard)
 
 // В блок .popup__type выведите тип жилья offer.type, сопоставив с подписями:
 // Квартира для flat
@@ -47,12 +70,8 @@ for(let i=0; i < similarPosters.length; i++){
 // Дворец для palace
 
 
-// В список .popup__features выведите все доступные удобства в объявлении.
-// В блок .popup__description выведите описание объекта недвижимости offer.description.
-// В блок .popup__photos выведите все фотографии из списка offer.photos. Каждая из строк массива photos должна записываться как src соответствующего изображения.
-// Замените src у аватарки пользователя — изображения, которое записано в .popup__avatar — на значения поля author.avatar отрисовываемого объекта.
+// В блок .popup__photos выведите все фотографии из списка offer.photos.
+// Каждая из строк массива photos должна записываться как src соответствующего изображения.
+
+
 // Если данных для заполнения не хватает, соответствующий блок в карточке скрывается.
-
-// Отрисуйте один из сгенерированных DOM-элементов, например первый, в блок #map-canvas, чтобы проверить, что данные в разметку были вставлены корректно.
-
-// Подключите модуль в проект.
