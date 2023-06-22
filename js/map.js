7547ca37a0a88d02b39e17083456008b1bd46195
Kryazh113 <skat_113@mail.ru>
2023-06-22 17:04:15 +0300

    add main marker, adress value

--- js/map.js
+++ js/map.js
@@ -28,21 +28,21 @@ const markerIcon = L.icon({
   iconAnchor: [20, 40],
 });
 
-const marker = L.marker(
-  {
-    lat: 35.6895000,
-    lng: 139.6917100,
-  },
-  {
-    icon: markerIcon,
-    draggable: true,
-  },
-);
-marker.addTo(map);
+// const marker = L.marker(
+//   {
+//     lat: 35.6895000,
+//     lng: 139.6917100,
+//   },
+//   {
+//     icon: markerIcon,
+//     draggable: true,
+//   },
+// );
+// marker.addTo(map);
 
 const mainMarker = L.marker(
   {
-    lat: 35.6910000,
+    lat: 35.6895000,
     lng: 139.6917100,
   },
   {
@@ -51,5 +51,18 @@ const mainMarker = L.marker(
   },
 );
 mainMarker.addTo(map);
+
+const inputAddress = formDescription.querySelector('#address');
+inputAddress.value = '35.68950, 139.69171';
+
+mainMarker.on('moveend', (evt) => {
+  const newCoordinate = evt.target.getLatLng();
+  const coordinateValue = Object.values(newCoordinate);
+  let toFixedCoorditate = [];
+  for(let i=0;i<coordinateValue.length;i++){
+    toFixedCoorditate[i]=coordinateValue[i].toFixed(5)
+  }
+  inputAddress.value = toFixedCoorditate;
+});
 
 
